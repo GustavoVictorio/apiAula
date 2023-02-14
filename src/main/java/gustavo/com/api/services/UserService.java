1d207e0a0cffa2f7cbdf7ee6727afdbfd80ec8e7
GustavoVictorio <gu.victorio3@gmail.com>
2023-02-13 21:26:23 -0300

    created findAll for user

--- src/main/java/gustavo/com/api/services/UserService.java
+++ src/main/java/gustavo/com/api/services/UserService.java
@@ -2,8 +2,12 @@ package gustavo.com.api.services;
 
 import gustavo.com.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 
 }
